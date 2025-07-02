55b533edb94051eda6647bcb2696c205fdceecc3
Parvez Ali <mohammadparvez4953@gmail.com>
2025-07-02 19:39:26 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+API_KEY = os.getenv("API_KEY")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/weather', methods=['POST'])
+def weather():
+    city = request.form['city']
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+
+    if data["cod"] != "404":
+        weather_data = {
+            "city": city,
+            "temperature": data["main"]["temp"],
+            "description": data["weather"][0]["description"],
+            "humidity": data["main"]["humidity"],
+            "wind_speed": data["wind"]["speed"],
+             
+            
+           
+        }
+        return render_template("result.html", weather=weather_data)
+    else:
+        return render_template("result.html", weather=None)
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=5000, debug=True)
