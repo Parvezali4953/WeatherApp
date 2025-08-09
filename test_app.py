8cb4d86ec22bd9c57db926a86f3087bc05c7dd1c
Parvezali4953 <mohammadparvez4953@gmail.com>
2025-08-09 20:05:16 +0530

    Adding test_app.py

--- test_app.py
+++ test_app.py
@@ -1,19 +1,17 @@
 import pytest
 from app import app
 
-
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-
-def test_health_check(client, monkeypatch):
+def test_health_check(client, mocker):
     """
     Test the /health endpoint to ensure it returns a 200 OK status.
     """
-    monkeypatch.setenv('API_KEY', 'FAKE_API_KEY')
+    mocker.patch.dict('os.environ', {'API_KEY': 'FAKE_API_KEY'})
 
     response = client.get('/health')
     assert response.status_code == 200
