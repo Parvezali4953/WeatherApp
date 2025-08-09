5fb918b10e14e3b5c28c9b0ee9ef1514f725e9ac
Parvezali4953 <mohammadparvez4953@gmail.com>
2025-08-09 16:33:03 +0530

    Configured Terraform for GitHub OIDC

--- infra/iam.tf
+++ infra/iam.tf
@@ -153,3 +153,11 @@ resource "aws_iam_role_policy" "github_actions_ecs" {
     ]
   })
 }
+
+# OpenID Connect Provider for GitHub Actions
+# This allows GitHub Actions to authenticate with AWS
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
