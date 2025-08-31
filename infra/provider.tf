e1725003511179a41a5bed00328a1f18d59442be
Parvezali4953 <mohammadparvez4953@gmail.com>
2025-08-31 22:31:56 +0530

    Update Workflows files

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "weatherapp-state-bucket-01"
+    bucket         = "weatherapp-state-bucket"
     key            = "infra/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "weatherapp-lock_table"
