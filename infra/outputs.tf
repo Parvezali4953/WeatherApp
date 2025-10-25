a4653be8f45b42453a29e046544c2ed3dfe250b0
Parvezali4953 <mohammadparvez4953@gmail.com>
2025-10-25 22:24:28 +0530

    Update infra/outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -20,3 +20,6 @@ output "ecs_cluster_name" {
 output "ecs_service_name" {
   value = module.ecs.ecs_service_name 
 }
+output "ecs_task_definition_arn" {
+  value = module.ecs.ecs_task_definition_arn
+}
